c6865dcf22f78ceaec8d852c3617b5e3aebed249
Ramanarayanan K <RAMANARAYANANK.IN@GMAIL.COM>
2023-07-17 22:22:16 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,14 +1,3 @@
 # Display a welcome message
 Write-Host "Welcome to the PowerShell sample script!"
 
-# Prompt the user for their name
-$name = Read-Host "Please enter your name:"
-
-# Display a personalized greeting
-Write-Host "Hello, $name! This is your first PowerShell script."
-
-# Get the current date and time
-$currentTime = Get-Date
-
-# Display the current date and time
-Write-Host "The current date and time is: $currentTime"
